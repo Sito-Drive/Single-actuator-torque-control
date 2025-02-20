a17ff0529ac20bdb0a6e554fc969631ae5b2a0fd
DrinkMoreHotWater <huangzhenwen@jacode.cn>
2025-02-20 17:08:49 +0800

    更改功能包和文件的名称，将数据文件路径改为相对功能包的相对路径

--- install/local_setup.sh
+++ install/local_setup.sh
@@ -3,7 +3,7 @@
 
 # since this file is sourced either use the provided _CATKIN_SETUP_DIR
 # or fall back to the destination set at configure time
-: ${_CATKIN_SETUP_DIR:=/home/hzw/ros_torque_control/Ws_Saber_750d/install}
+: ${_CATKIN_SETUP_DIR:=/home/hzw/ros_torque_control/ws_saber_750d/install}
 CATKIN_SETUP_UTIL_ARGS="--extend --local"
 . "$_CATKIN_SETUP_DIR/setup.sh"
 unset CATKIN_SETUP_UTIL_ARGS
--- install/share/single_actuator/cmake/single_actuatorConfig.cmake
+++ install/share/single_actuator/cmake/single_actuatorConfig.cmake
@@ -67,14 +67,14 @@ set(single_actuator_CONFIG_INCLUDED TRUE)
 
 # set variables for source/devel/install prefixes
 if("FALSE" STREQUAL "TRUE")
-  set(single_actuator_SOURCE_PREFIX /home/hzw/ros_torque_control/Ws_Saber_750d/src/single_actuator)
-  set(single_actuator_DEVEL_PREFIX /home/hzw/ros_torque_control/Ws_Saber_750d/devel)
+  set(single_actuator_SOURCE_PREFIX /home/hzw/ros_torque_control/ws_saber_750d/src/single_actuator)
+  set(single_actuator_DEVEL_PREFIX /home/hzw/ros_torque_control/ws_saber_750d/devel)
   set(single_actuator_INSTALL_PREFIX "")
   set(single_actuator_PREFIX ${single_actuator_DEVEL_PREFIX})
 else()
   set(single_actuator_SOURCE_PREFIX "")
   set(single_actuator_DEVEL_PREFIX "")
-  set(single_actuator_INSTALL_PREFIX /home/hzw/ros_torque_control/Ws_Saber_750d/install)
+  set(single_actuator_INSTALL_PREFIX /home/hzw/ros_torque_control/ws_saber_750d/install)
   set(single_actuator_PREFIX ${single_actuator_INSTALL_PREFIX})
 endif()
 
@@ -154,7 +154,7 @@ foreach(library ${libraries})
     set(lib_path "")
     set(lib "${library}-NOTFOUND")
     # since the path where the library is found is returned we have to iterate over the paths manually
-    foreach(path /home/hzw/ros_torque_control/Ws_Saber_750d/install/lib;/opt/ros/noetic/lib)
+    foreach(path /home/hzw/ros_torque_control/ws_saber_750d/install/lib;/opt/ros/noetic/lib)
       find_library(lib ${library}
         PATHS ${path}
         NO_DEFAULT_PATH NO_CMAKE_FIND_ROOT_PATH)
@@ -185,7 +185,7 @@ foreach(t ${single_actuator_EXPORTED_TARGETS})
   endif()
 endforeach()
 
-set(depends "roscpp;rospy;std_msgs")
+set(depends "roscpp;rospy;std_msgs;message_runtime")
 foreach(depend ${depends})
   string(REPLACE " " ";" depend_list ${depend})
   # the package name of the dependency must be kept in a unique variable so that it is not overwritten in recursive calls
