475c177d0c9d02f9704d2a04bc395ec1ca6fcd99
DrinkMoreHotWater <huangzhenwen@jacode.cn>
2025-04-28 16:53:08 +0800

    增加一对摩擦与力矩的关系的摩擦系数

--- install/share/control_interface/cmake/control_interfaceConfig.cmake
+++ install/share/control_interface/cmake/control_interfaceConfig.cmake
@@ -154,7 +154,7 @@ foreach(library ${libraries})
     set(lib_path "")
     set(lib "${library}-NOTFOUND")
     # since the path where the library is found is returned we have to iterate over the paths manually
-    foreach(path /home/hzw/ros_torque_control/ws_saber_750d/install/lib;/opt/ros/noetic/lib)
+    foreach(path /home/hzw/ros_torque_control/ws_saber_750d/install/lib;/home/hzw/ros_torque_control/ws_saber_750d/devel/lib;/opt/ros/noetic/lib)
       find_library(lib ${library}
         PATHS ${path}
         NO_DEFAULT_PATH NO_CMAKE_FIND_ROOT_PATH)
